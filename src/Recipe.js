2e220ce168539ea2243b07b72b0221a014665685
Mohamed Ragab <38765123+Mohamed-Ragab-Tawfiek@users.noreply.github.com>
2019-07-30 14:34:46 +0200

    improve calories and ingredients style

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,18 +1,19 @@
 import React from "react";
 import Style from "./recipe.module.css";
-const Recipe = ({title,calories,image,ingredients}) => {
+const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div className={Style.recipe}>
       <h1>{title}</h1>
       <ol>
-        {ingredients.map(ingredient =>(
+        <h4>Ingredients :</h4>
+        {ingredients.map(ingredient => (
           <li>{ingredient.text}</li>
         ))}
       </ol>
-      <p>{calories}</p>
+      <p><strong>Calories: </strong>{ Math.round(calories/100)*100}</p>
       <img className={Style.image} src={image} alt="" />
     </div>
   );
-}
+};
 
 export default Recipe;
